b220130f847eca5fd57f7b85b1300c7c933cf525
Santiago Narvaez Rivas <santiagonar1@gmail.com>
2016-03-11 17:35:48 -0500

    Primer commit

---
+++ 007-10001st_prime.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Problem description:
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+
+Author: Santiago Narváez Rivas.
+Date: 11-Mar-2016
+"""
+
+N_PRIME = 10001
+
+def gen_primes():
+    """ Generate an infinite sequence of prime numbers.
+    """
+    # Maps composites to primes witnessing their compositeness.
+    # This is memory efficient, as the sieve is not "run forward"
+    # indefinitely, but only as long as required by the current
+    # number being tested.
+    #
+    D = {}
+
+    # The running integer that's checked for primeness
+    q = 2
+
+    while True:
+        if q not in D:
+            # q is a new prime.
+            # Yield it and mark its first multiple that isn't
+            # already marked in previous iterations
+            #
+            yield q
+            D[q * q] = [q]
+        else:
+            # q is composite. D[q] is the list of primes that
+            # divide it. Since we've reached q, we no longer
+            # need it in the map, but we'll mark the next
+            # multiples of its witnesses to prepare for larger
+            # numbers
+            #
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+
+        q += 1
+
+if __name__ == "__main__":
+    n = 1
+    for p in gen_primes():
+        if n == N_PRIME:
+            print("Prime {0}: {1}".format(N_PRIME, p))
+            break
+        n += 1
